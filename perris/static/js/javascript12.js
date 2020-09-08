8d4b98962559345582557803a412412c26b63447
Carlos Alvarez Peña <43711230+wulfkarl@users.noreply.github.com>
2020-09-08 00:20:13 -0300

    Add files via upload

---
+++ perris/static/js/javascript12.js
@@ -0,0 +1,59 @@
+//Perro Oso
+$("#boso").onclick(function () {
+    $('#modal1').modal({
+        show: true,
+        backdrop: false
+    })
+});
+
+//Perro Maya
+$("#bmaya").onclick(function () {
+    $('#modal1').modal({
+        show: true,
+        backdrop: false
+    })
+});
+
+//Perro Wifi
+$("#bwifi").onclick(function () {
+    $('#modal1').modal({
+        show: true,
+        backdrop: false
+    })
+});
+
+
+
+//Perro Chocolate
+$("#bchocolate").onclick(function () {
+    $('#modal1').modal({
+        show: true,
+        backdrop: false
+    })
+});
+
+//Perro Pexel
+$("#bpexel").onclick(function () {
+    $('#modal1').modal({
+        show: true,
+        backdrop: false
+    })
+});
+
+
+
+//Perro Bigotes
+$("#bbigotes").onclick(function () {
+    $('#modal1').modal({
+        show: true,
+        backdrop: false
+    })
+});
+
+//Perro Luna
+$("#bluna").onclick(function () {
+    $('#modal1').modal({
+        show: true,
+        backdrop: false
+    })
+});
